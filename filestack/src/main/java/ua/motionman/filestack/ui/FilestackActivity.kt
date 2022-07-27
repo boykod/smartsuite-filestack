eacde70fb3a12321ab1a2381dbd0476903c8e9a0
motionman <dmytro.hutsulenko@ekreative.com>
2022-07-27 12:23:39 +0300

    fix: update handle back press

--- filestack/src/main/java/ua/motionman/filestack/ui/FilestackActivity.kt
+++ filestack/src/main/java/ua/motionman/filestack/ui/FilestackActivity.kt
@@ -2,6 +2,8 @@ package ua.motionman.filestack.ui
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import ua.motionman.filestack.R
 import ua.motionman.filestack.databinding.FilestackActivityBinding
 import ua.motionman.filestack.utils.client.ClientProvider
 import ua.motionman.filestack.utils.delegate.viewBinding
@@ -24,6 +26,17 @@ class FilestackActivity : AppCompatActivity() {
 
         if (!apiKey.isNullOrEmpty() && !policy.isNullOrEmpty() && !signature.isNullOrEmpty()) {
             ClientProvider.instance.initClient(apiKey, policy, signature)
+        }
+    }
+
+    override fun onBackPressed() {
+        val currentDestinationId = findNavController(R.id.nav_host_fragment).currentDestination?.id
+
+        if (currentDestinationId != null) {
+            when (currentDestinationId) {
+                R.id.uploadingProgressFragment -> {}
+                else -> super.onBackPressed()
+            }
         }
     }
 
--- filestack/src/main/java/ua/motionman/filestack/ui/uploadingprogress/UploadingProgressFragment.kt
+++ filestack/src/main/java/ua/motionman/filestack/ui/uploadingprogress/UploadingProgressFragment.kt
@@ -4,7 +4,6 @@ import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
 import android.view.View
-import androidx.activity.addCallback
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.lifecycleScope
@@ -28,7 +27,6 @@ class UploadingProgressFragment : Fragment(R.layout.uploading_progress) {
         setupListeners()
         initFlowSubscriber()
         startUploadService()
-        disableOnBackPress()
     }
 
     private fun setupListeners() {
@@ -69,12 +67,6 @@ class UploadingProgressFragment : Fragment(R.layout.uploading_progress) {
         }
         activity?.setResult(Activity.RESULT_OK, intent)
         activity?.finish()
-    }
-
-    private fun disableOnBackPress() {
-        activity?.onBackPressedDispatcher?.addCallback {
-            // do nothing
-        }
     }
 
     companion object {
